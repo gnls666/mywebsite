446065383148eaa6246f897ca1ab7135d7cb6382
gnls666 <185263317@qq.com>
2024-05-29 16:46:30 +0800

    私人化

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import Link, { type LinkProps } from 'next/link'
 
 // import portraitImage from '~/assets/Portrait.png'
-import portraitImage from '~/assets/avatar2.png'
+import portraitImage from '~/assets/avatar.png'
 import portraitAltImage from '~/assets/PortraitAlt.jpg'
 
 function AvatarContainer({ className, ...props }: ComponentProps) {
